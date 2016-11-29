91b2553f4a6bd6f9e0dacacb9da64494569ac9bc
Guillaume Desrumaux <guillaume.desrumaux@isen.yncrea.fr>
2016-11-29 09:20:57 +0100

    Ajout de la connexion à la base données (MySQL & PSQL)

---
+++ App/Config/config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Guillaume Desrumaux
+ * @date 28/11/16
+ */
+
+/**
+ * @const Mysql Host
+ */
+define('DB_HOST', 'db_mysql');
+/**
+ * @const Mysql user
+ */
+define('DB_USER', 'root');
+/**
+ * @const Mysql password
+ */
+define('DB_PASSWORD', 'root');
+/**
+ * @const Mysql port
+ */
+define('DB_PORT', '3306');
