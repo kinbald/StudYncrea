1046bf7e3c660840a001bb150c11e4b1ecbde8df
Desrumaux <guillaume.desrumaux@isen.fr>
2016-12-11 16:50:09 +0100

    Modification du Loader de classes

--- App/App.php
+++ App/App.php
@@ -1,32 +1,44 @@
 <?php
-
-/**
- * Fichier de configuration où sont stockés les constantes de connexion
- */
-require 'Config/config.php';
-
-/**
- * @author Guillaume Desrumaux
- * @date 29/11/16
- */
-class App
-{
     /**
-     * @var Database Instance de base de donnnées
+     * Fichier de configuration où sont stockés les constantes de connexion
      */
-    private static $database;
-
+    use Core\Database\MysqlDatabase;
+    
+    require '../Config/config.php';
+    
     /**
-     * Fonction qui retourne une instance de base de données
-     * @return Database Instance retournée
+     * @author Guillaume Desrumaux
+     * @date 29/11/16
      */
-    public static function getDb()
+    class App
     {
-        if(self::$database === null)
+        /**
+         * @var Database Instance de base de donnnées
+         */
+        private static $database;
+        
+        /**
+         * Fonction qui retourne une instance de base de données
+         * @return Database Instance retournée
+         */
+        public static function getDb()
         {
-            self::$database = new Database(DB_NAME);
-            //self::$database = self::$database->getConnectionMySQL();
+            if(self::$database === null)
+            {
+                self::$database = new MysqlDatabase(DB_NAME);
+            }
+            return self::$database;
         }
-        return self::$database;
+        
+        /**
+         * Fonction permettant de charger toutes les classes
+         */
+        public static function load()
+        {
+            //TODO Add Session Start
+            require 'Autoload.php';
+            App\Autoload::register();
+            require '../Core/Autoload.php';
+            \Core\Autoload::register();
+        }
     }
-}
