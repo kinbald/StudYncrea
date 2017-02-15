d7d3d13a216bdabeac2aaa55322d360f19275a46
Félix <felix.herrenschmidt@isen.fr>
2017-02-15 18:41:59 +0100

    Ajout gestion PDF

--- public/post.php
+++ public/post.php
@@ -30,29 +30,35 @@ else
                             <br>
                             <br>
                             <p><?= $post['description'] ?></p>
-                            <br> 
                             <div class="row">             
                                 <?php
                                 if (!empty($post['url_file']))
                                 {
                                     ?>
-                                    <div class="divider"></div>
                                     <br>
                                     <div class="row">
                                         <div class="col s12">
-                                            <div class="col s6 m3 l2">
-                                                <img class="left responsive-img materialboxed" width="150" src="<?= $post['url_file'] ?>">
-                                            </div>
-
                                             <?php
-
-                                            if (!empty($post['url_file_secondary']))
+                                            list($debut, $milieu, $fin ,$extension_file) = explode(".",$post['url_file']);
+                                            if ($extension_file != "pdf")
                                             {
                                                 ?>
                                                 <div class="col s6 m3 l2">
-                                                    <img class="responsive-img materialboxed" width="150" src="<?= $post['url_file_secondary'] ?>">   
-                                                </div>                          
-                                                <?php        
+                                                    <img class="left responsive-img materialboxed" width="150" src="<?= $post['url_file'] ?>">
+                                                </div>
+                                                <?php 
+                                            }    
+                                            if (!empty($post['url_file_secondary']))
+                                            {
+                                                list($debut, $milieu, $fin ,$extension_file_secondary) = explode(".",$post['url_file_secondary']);
+                                                if ($extension_file_secondary != "pdf")
+                                                {
+                                                    ?>
+                                                    <div class="col s6 m3 l2">
+                                                        <img class="responsive-img materialboxed" width="150" src="<?= $post['url_file_secondary'] ?>">   
+                                                    </div>                          
+                                                    <?php  
+                                                }                                              
                                             }
                                             ?>
                                         </div>
@@ -61,44 +67,64 @@ else
                                 }
                                 if (!empty($post['url_file']))
                                 {
+                                 ?><div class="card-action"><?php
+                                 if ($extension_file != "pdf")
+                                 {
                                     ?>
-                                    <div class="card-action">
+                                    <div class="col s7 m3 l2">
+                                        <a class="waves-effect waves-teal btn-flat black-text tooltipped" data-position="top" data-delay="50" data-tooltip="Ouvrir l'image" target="_blank" href="<?= $post['url_file'] ?>"><i class="material-icons">photo_camera</i></a>
+                                        </div>
+                                        <?php
+                                    }   
+                                    else
+                                    {
+                                        ?>
                                         <div class="col s6 m3 l2">
-                                            <a class="waves-effect waves-teal btn-flat black-text tooltipped" data-position="top" data-delay="50" data-tooltip="Ouvrir" target="_blank" href="<?= $post['url_file'] ?>"><i class="material-icons left">
-                                                keyboard_arrow_left</i></a>
-                                            </div>
-                                            <?php
-
-                                            if (!empty($post['url_file_secondary']))
-                                            {
-                                                ?>
-                                                <div class="col s6 m3 l2">
-                                                    <a class="waves-effect waves-teal btn-flat black-text tooltipped" data-position="top" data-delay="50" data-tooltip="Ouvrir" target="_blank" href="<?= $post['url_file_secondary'] ?>"><i class="material-icons right">keyboard_arrow_right</i></a>
-                                                </div>
-                                                <?php
-                                            }
-                                            ?>
+                                            <a class="waves-effect waves-teal btn-flat black-text tooltipped" data-position="top" data-delay="50" data-tooltip="Ouvrir le PDF" target="_blank" href="<?= $post['url_file'] ?>"><i class="material-icons">picture_as_pdf</i></a>
                                         </div>
                                         <?php
                                     }
+                                    if (!empty($post['url_file_secondary']))
+                                    {
+                                        if ($extension_file_secondary != "pdf")
+                                        {
+                                            ?>
+                                            <div class="col s5 m3 l2">
+                                                <a class="waves-effect waves-teal btn-flat black-text tooltipped" data-position="top" data-delay="50" data-tooltip="Ouvrir l'image" target="_blank" href="<?= $post['url_file_secondary'] ?>"><i class="material-icons">photo_camera</i></a>
+                                            </div>
+                                            <?php
+                                        }   
+                                        else
+                                        {
+                                            ?>
+                                            <div class="col s6 m3 l2">
+                                                <a class="waves-effect waves-teal btn-flat black-text tooltipped" data-position="top" data-delay="50" data-tooltip="Ouvrir le PDF" target="_blank" href="<?= $post['url_file_secondary'] ?>"><i class="material-icons">picture_as_pdf</i></a>
+                                            </div>
+                                            <?php
+                                        }
+                                    }
                                     ?>
                                 </div>
-                                <a class="left grey-text"><?= $post['name_class'] ?></a>
-                                <a class="right grey-text"><?= $blog::display_date($post['date_post']); ?></a>
-                            </div>
+                                <?php
+                            }
+                            ?>
                         </div>
+                        <a class="left grey-text"><?= $post['name_class'] ?></a>
+                        <a class="right grey-text"><?= $blog::display_date($post['date_post']); ?></a>
                     </div>
                 </div>
-                <?php
-            }
-            else
-            {
-                header('Location: affichage_blog.php');
-            }
-            ?>
+            </div>
         </div>
         <?php
     }
+    else
+    {
+        header('Location: affichage_blog.php');
+    }
+    ?>
+</div>
+<?php
+}
 
 
-    include '../Vues/footer.php';
+include '../Vues/footer.php';
