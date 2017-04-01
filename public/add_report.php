be86b06e3d058cefa7ae63404f62d62665c7e8ae
Félix <felix.herrenschmidt@isen.fr>
2017-04-01 17:09:10 +0200

    Ajout envoie de SMS FreeMobile Signalements
    Ajout API freemobile pour envoyer un sms lors d'un signalement.

--- public/add_report.php
+++ public/add_report.php
@@ -10,6 +10,10 @@
     $auth = App::getAuth();
     $auth->restrict();
     include "../Vues/header.php";
+    // API Free 
+    require "../App/SmsFree.php";
+    $sms = new \App\SmsFree();
+
     if ($_GET)
     {
         if (isset($_GET['comment']))
@@ -17,7 +21,7 @@
             $id_comment = filter_input(INPUT_GET, 'comment');
             $Comment = new \App\Model\CommentModel(App::getDb());
             $element = $Comment->findBy('id_comment', [$id_comment], 1);
-            if ($element != FALSE)
+            if ($element != FALSE) 
             {
                 if ($element['is_online'] == 1)
                 {
@@ -33,33 +37,54 @@
                             $errors['type'] = 'Problème dans le choix du type de signalement';
                         }
                         if (!empty($errors))
-                        { ?>
-                            <div class="card red">
-                                <div class="card-content white-text">
-                                    <?php foreach ($errors as $error)
-                                    {
-                                        echo $error . "<br/>";
-                                    } ?>
-                                </div>
+                            { ?>
+                        <div class="card red">
+                            <div class="card-content white-text">
+                                <?php foreach ($errors as $error)
+                                {
+                                    echo $error . "<br/>";
+                                } ?>
                             </div>
-                            <?php
-                        }
-                        else
+                        </div>
+                        <?php
+                    }
+                    else
+                    {
+                        if ($_POST['type_report'] == 'radio-inaprop')
                         {
-                            if ($_POST['type_report'] == 'radio-inaprop')
-                            {
-                                $type_report = 0;
+                            $type_report = 0;
+                        }
+                        elseif ($_POST['type_report'] == 'radio-discrim')
+                        {
+                            $type_report = 1;
+                        }
+                        elseif ($_POST['type_report'] == 'radio-spam')
+                        {
+                            $type_report = 2;
+                        }
+                        $Report->addReport($_POST['reason'], $type_report, App::getAuth()->getUser()['id_user'], null, $id_comment);
+                        App::getAuth()->getSession()->setFlash('success', 'Votre signalement à bien été pris en compte');
+                        
+                            // ----------------------------------Envoie Notification Report SMS FREEMOBILE
+                            /**
+                             * Configure l'ID utilisateur (sur la facture FREEMOBILE) et la clé disponible dans
+                             * le compte Free Mobile après avoir activé l'option.
+                             */
+                            die ("Ce 'die();' est normal il faut Remplir la CLÉ et l'ID utilisateur FREEMOBILE pour gérer l'envoi de SMS dans add_report.php ligne 206 et 73");
+                            $sms->setKey("La clé générée")->setUser("Votre id Free Mobile");//--------------ICI
+
+                            try {
+                                $lienPost = 'http://localhost/StudYncrea/public/post.php?post='.$element['id_post'];
+                                // envoi d'un message
+                                $userPost = App::getAuth()->getUser()['email'];
+                                $sms->send('Signalement de '.$userPost.' sur un commentaire du post: '. $lienPost);
+                            } catch (Exception $e) {
+                            // le monde n'est pas parfait, il y aura
+                            // peut-être des erreurs.
+                                echo "Erreur sur envoi de SMS: (".$e->getCode().") ".$e->getMessage();
                             }
-                            elseif ($_POST['type_report'] == 'radio-discrim')
-                            {
-                                $type_report = 1;
-                            }
-                            elseif ($_POST['type_report'] == 'radio-spam')
-                            {
-                                $type_report = 2;
-                            }
-                            $Report->addReport($_POST['reason'], $type_report, App::getAuth()->getUser()['id_user'], null, $id_comment);
-                            App::getAuth()->getSession()->setFlash('success', 'Votre signalement à bien été pris en compte');
+                            // ----------------------------------
+
                             App::redirect('post.php?post=' . $element['id_post']);
                         }
                     }
@@ -81,7 +106,7 @@
                     <div class="card-panel">
                         <form id="signaler" action="" method="post" class="col s12" enctype="multipart/form-data">
                             <?php
-                                $form->textArea('reason', 'Raison du signalement', true);
+                            $form->textArea('reason', 'Raison du signalement', true);
                             ?>
                             <p>
                                 Nature du signalement :
@@ -99,7 +124,7 @@
                                 <label for="radio-spam">Spam</label>
                             </p>
                             <?php
-                                $form->submit('Signaler la publication');
+                            $form->submit('Signaler la publication');
                             ?>
                         </form>
                     </div>
@@ -145,92 +170,113 @@
                                 $errors['type'] = 'Problème dans le choix du type de signalement';
                             }
                             if (!empty($errors))
-                            { ?>
-                                <div class="card red">
-                                    <div class="card-content white-text">
-                                        <?php foreach ($errors as $error)
-                                        {
-                                            echo $error . "<br/>";
-                                        } ?>
-                                    </div>
+                                { ?>
+                            <div class="card red">
+                                <div class="card-content white-text">
+                                    <?php foreach ($errors as $error)
+                                    {
+                                        echo $error . "<br/>";
+                                    } ?>
                                 </div>
-                                <?php
-                            }
-                            else
-                            {
-                                if ($_POST['type_report'] == 'radio-inaprop')
-                                {
-                                    $type_report = 0;
-                                }
-                                elseif ($_POST['type_report'] == 'radio-discrim')
-                                {
-                                    $type_report = 1;
-                                }
-                                elseif ($_POST['type_report'] == 'radio-spam')
-                                {
-                                    $type_report = 2;
-                                }
-                                $Report->addReport($_POST['reason'], $type_report, App::getAuth()->getUser()['id_user'], $id_post, null);
-                                App::redirect('post.php?post=' . $id_post);
-                            }
-
+                            </div>
+                            <?php
                         }
-                    }
-                    else
-                    {
-                        App::redirect('affichage_blog.php');
+                        else
+                        {
+                            if ($_POST['type_report'] == 'radio-inaprop')
+                            {
+                                $type_report = 0;
+                            }
+                            elseif ($_POST['type_report'] == 'radio-discrim')
+                            {
+                                $type_report = 1;
+                            }
+                            elseif ($_POST['type_report'] == 'radio-spam')
+                            {
+                                $type_report = 2;
+                            }
+                            $Report->addReport($_POST['reason'], $type_report, App::getAuth()->getUser()['id_user'], $id_post, null);
+
+                            // ----------------------------------Envoie Notification Report SMS FREEMOBILE
+                            /**
+                             * Configure l'ID utilisateur (sur la facture FREEMOBILE) et la clé disponible dans
+                             * le compte Free Mobile après avoir activé l'option.
+                             */
+                            die ("Ce 'die();' est normal il faut Remplir la CLÉ et l'ID utilisateur FREEMOBILE pour gérer l'envoi de SMS dans add_report.php ligne 206 et 73");
+                            $sms->setKey("La clé générée")->setUser("Votre id Free Mobile");//--------------ICI
+
+                            try {
+                                $lienPost = 'http://localhost/StudYncrea/public/post.php?post='.$id_post;
+                                // envoi d'un message
+                                $userPost = App::getAuth()->getUser()['email'];
+                                $sms->send('Signalement de '.$userPost.' sur le post: '. $lienPost);
+                            } catch (Exception $e) {
+                            // le monde n'est pas parfait, il y aura
+                            // peut-être des erreurs.
+                                echo "Erreur sur envoi de SMS: (".$e->getCode().") ".$e->getMessage();
+                            }
+                            // ----------------------------------
+
+                            App::redirect('post.php?post=' . $id_post);
+                        }
+
                     }
                 }
                 else
                 {
                     App::redirect('affichage_blog.php');
                 }
-                $form = new \App\Form();
-                ?>
-                <div class="container">
-                    <div class="section">
-                        <h1>Signaler la publication : </h1>
-                        <div class="card-panel">
-                            <form id="signaler" action="" method="post" class="col s12" enctype="multipart/form-data">
-                                <?php
-                                    $form->textArea('reason', 'Raison du signalement', true);
-                                ?>
-                                <p>
-                                    Nature du signalement :
-                                </p>
-                                <p>
-                                    <input name="type_report" type="radio" id="radio-inaprop" value="radio-inaprop"/>
-                                    <label for="radio-inaprop">Langage inapproprié</label>
-                                </p>
-                                <p>
-                                    <input name="type_report" type="radio" id="radio-discrim" value="radio-discrim"/>
-                                    <label for="radio-discrim">Contenu offensant ou haineux</label>
-                                </p>
-                                <p>
-                                    <input name="type_report" type="radio" id="radio-spam" value="radio-spam"/>
-                                    <label for="radio-spam">Spam</label>
-                                </p>
-                                <?php
-                                    $form->submit('Signaler la publication');
-                                ?>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <?php
-                include '../Vues/footer.php';
             }
             else
             {
-                App::redirect('post.php?post=' . $id_post);
+                App::redirect('affichage_blog.php');
             }
+            $form = new \App\Form();
+            ?>
+            <div class="container">
+                <div class="section">
+                    <h1>Signaler la publication : </h1>
+                    <div class="card-panel">
+                        <form id="signaler" action="" method="post" class="col s12" enctype="multipart/form-data">
+                            <?php
+                            $form->textArea('reason', 'Raison du signalement', true);
+                            ?>
+                            <p>
+                                Nature du signalement :
+                            </p>
+                            <p>
+                                <input name="type_report" type="radio" id="radio-inaprop" value="radio-inaprop"/>
+                                <label for="radio-inaprop">Langage inapproprié</label>
+                            </p>
+                            <p>
+                                <input name="type_report" type="radio" id="radio-discrim" value="radio-discrim"/>
+                                <label for="radio-discrim">Contenu offensant ou haineux</label>
+                            </p>
+                            <p>
+                                <input name="type_report" type="radio" id="radio-spam" value="radio-spam"/>
+                                <label for="radio-spam">Spam</label>
+                            </p>
+                            <?php
+                            $form->submit('Signaler la publication');
+                            ?>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <?php
+            include '../Vues/footer.php';
         }
         else
         {
-            App::redirect('affichage_blog.php');
+            App::redirect('post.php?post=' . $id_post);
         }
     }
     else
     {
         App::redirect('affichage_blog.php');
     }
+}
+else
+{
+    App::redirect('affichage_blog.php');
+}
